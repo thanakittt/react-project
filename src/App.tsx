fa322f7c26aca65c0457994aa815e5d2df6c6a34
bamboo513 <bamboobg14@gmail.com>
2026-03-20 14:05:33 +0700

    feat: เปลี่ยนข้อความ

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 export default function App() {
   return (
     <div className="flex justify-center items-center h-screen">
-      <h1 className="text-3xl font-bold underline text-red-500">
-        Hello world!
+      <h1 className="text-4xl font-bold  text-red-500">
+        Hi!!
       </h1>
     </div>
   );
