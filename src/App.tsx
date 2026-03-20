bedf13b5725c694cb7dd72fb0f08c614ce7d3f54
bamboo513 <bamboobg14@gmail.com>
2026-03-20 14:30:21 +0700

    feat: เพิ่ม card ลงใน App

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,16 @@ import Button from "./Button";
 export default function App() {
   return (
     <div className="flex justify-center items-center h-screen flex-col gap-5">
+      <Card />
       <h1 className="text-4xl font-bold  text-red-500">Hello, BamBoo</h1>
       <Button />
+    </div>
+  );
+}
+function Card() {
+  return (
+    <div className="">
+      <h1 className="text-4xl font-bold  text-red-500">Hello, Frame</h1>
     </div>
   );
 }
