6dcf3ce5574c63fc0d041f5fcc67e2b6cac981c4
bamboo513 <bamboobg14@gmail.com>
2026-03-20 16:10:20 +0700

    feat: add style form login

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,10 @@ export default function App() {
 function LoginForm() {
   return (
     <div className="flex justify-center items-center h-screen flex-col gap-5">
-      <h1 className="text-4xl font-bold  text-black">Login</h1>
-      <input type="text" placeholder="Username" />
-      <input type="password" placeholder="Password" />
-      <button>Login</button>
+      <h1 className="text-4xl font-bold  text-black uppercase">Login</h1>
+      <input type="text" placeholder="Username" className="border-1 border-black rounded-lg p-2 w-60" />
+      <input type="password" placeholder="Password" className="border-1 border-black rounded-lg p-2 w-60" />
+      <button className="border-1 border-gray-400 rounded-lg p-2 w-20 bg-blue-500 text-white">Login</button>
     </div>
   );
 }
