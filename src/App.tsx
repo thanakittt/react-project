fd0ab24b2a146bf0fff2045a99a109bfd16d48b2
bamboo513 <bamboobg14@gmail.com>
2026-03-20 14:52:12 +0700

    Revert "feat: change color for Card ,bg tx"
    This reverts commit 89a59e810cd4509f9c8e595b99b5bf9ffdb3f028.

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,8 @@ export default function App() {
 }
 function Card() {
   return (
-    <div className="bg-red-500 p-5 rounded-lg">
-      <h1 className="text-4xl font-bold  text-white">Hello, Frame</h1>
+    <div className="">
+      <h1 className="text-4xl font-bold  text-red-500">Hello, Frame</h1>
     </div>
   );
 }
