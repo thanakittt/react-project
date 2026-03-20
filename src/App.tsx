2b2091d1c9f7b6893f26e6426336bc4aaa232adb
Thanakit2023 <150004716+thanakittt@users.noreply.github.com>
2026-03-20 14:16:09 +0700

    feat: Add Button component and update App UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import Button from "./Button";
+
 export default function App() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <h1 className="text-4xl font-bold  text-red-500">
-        Hi!!
-      </h1>
+    <div className="flex justify-center items-center h-screen flex-col gap-5">
+      <h1 className="text-4xl font-bold  text-red-500">Hello, BamBoo</h1>
+      <Button />
     </div>
   );
 }
